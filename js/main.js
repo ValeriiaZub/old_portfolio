2c2569b7b58ba1034002fd42f57987b698607eb2
Alec Di Vito <divito.alec@gmail.com>
2020-02-09 21:59:34 -0500

    added x tilt to model

--- js/main.js
+++ js/main.js
@@ -10,13 +10,26 @@ $(document).ready(function () {
             });
         }
     });
-
 });
 
-let canvas, introObj;
+let canvas, texture, introObj;
+let mouseX, mouseY;
+let windowX, windowY;
+
+window.addEventListener("mousemove", (ev) => {
+    mouseX = ev.clientX;
+    mouseY = ev.clientY;
+})
+
+window.addEventListener("resize", (ev) => {
+    windowX = window.innerWidth;
+    windowY = window.innerHeight;
+})
 
 function preload() {
-    introObj = loadModel('/assets/intro.obj');
+    introObj = loadModel('/assets/intro2.obj');
+    windowX = window.innerWidth;
+    windowY = window.innerHeight;
 }
 
 function setup() {
@@ -27,9 +40,18 @@ function setup() {
 
 function draw() {
     background(200);
-    translate(width / 2, height / 2);
-    translate(p5.Vector.fromAngle(millis() / 1000, 40));
+
+    // figure out x rotation
+    const x_rotate = ((mouseY / (windowY / 2)) - 1) * -1;
+    const y_rotate = ((mouseX / (windowX / 2)) - 1) * -1;
+
+    rotateX(PI);
+    translate(-1350, 0, 1200);
+    rotateX(x_rotate);
+    // rotateY(y_rotate);
+    // translate(p5.Vector.fromAngle(millis() / 1000, 40));
     // rotateX(frameCount * 0.01);
     // rotateY(frameCount * 0.01);
-    model(introObj)
+    normalMaterial();
+    model(introObj);
 }
